2df314e9c53a6d3ca80cc426f44052787653a499
Advaith Sanil Kumar <advaithsanilkumar@gmail.com>
2025-06-27 18:09:52 +0530

    Modified Prometheus handler tests to add debug prints

--- mindsdb/integrations/handlers/prometheus_handler/tests/test_prometheus_handler.py
+++ mindsdb/integrations/handlers/prometheus_handler/tests/test_prometheus_handler.py
@@ -12,26 +12,32 @@ class PrometheusHandlerTest(unittest.TestCase):
         cls.handler = PrometheusHandler('test_prometheus_handler', cls.kwargs)
 
     def test_0_check_connection(self):
-        assert self.handler.check_connection()
+        result = self.handler.check_connection()
+        print("\n\n--- DEBUG (check_connection) ---\n" + str(result))
+        assert result
 
     def test_1_get_tables(self):
         tables = self.handler.get_tables()
+        print("\n\n--- DEBUG (get_tables) ---\n" + str(tables))
         assert tables.type is not RESPONSE_TYPE.ERROR
 
     def test_2_get_columns(self):
         columns = self.handler.get_columns('baseballStats')
+        print("\n\n--- DEBUG (get_columns) ---\n" + str(columns))
         assert columns.type is not RESPONSE_TYPE.ERROR
 
     def test_3_native_query_select(self):
-        query = "prometheus_http_requests_total"
+        query = "topk(1, sum(rate(container_cpu_usage_seconds_total[30d])) by (cluster))"
         result = self.handler.native_query(query)
+        print("\n\n--- DEBUG (native_query_select) ---\n" + str(result))
         assert result.type is RESPONSE_TYPE.TABLE
 
     def test_4_native_query_select(self):
         query = "http_requests_total_fake"
         result = self.handler.native_query(query)
+        print("\n\n--- DEBUG (native_query_select_fake) ---\n" + str(result))
         assert result.type is RESPONSE_TYPE.TABLE
-
 
 if __name__ == '__main__':
     unittest.main()
+    
